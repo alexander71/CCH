6e5aab846c07b2bb22cdcd8962e2a4f07b1d0cd2
John Lowe <jblowe@berkeley.edu>
2017-07-19 11:40:05 -0700

    modify georef2.sql to wrote with revised georeference input files (11 columns...)

--- pl/georef2sql.pl
+++ pl/georef2sql.pl
@@ -2,51 +2,65 @@
 #Input file is a tab delimited table with the columns listed below
 #Georeferencer and georef_remarks are hardcoded into the printout
 
-#Accumulate results and send to Chris periodically
+#Accumulate results and send to Reseach IT / CSpace periodically
 
-    my $file = 'to_process/alpine_distance_kluge';
+# usage
+# perl georef2sql.pl HREC_georef.txt HREC_georef.sql
+# input:  HREC_georef.txt
+# output: HREC_georef.sql
+# Success. 1236 statements output
 
-open(OUT,">georef2sql.out") || die;;
-	
-open(IN,$file) || die;
+
+my $infile  = shift;
+my $outfile = shift;
+
+print "input:  $infile\n";
+print "output: $outfile\n";
+
+open(IN,"<$infile") || die "could not read from $infile";
+open(OUT,">$outfile") || die "could not write to $outfile" ;
+
 Record: while(<IN>){
-	chomp;
-	@columns=split(/\t/,$_,100);
-		unless( $#columns==6){
-		print ERR "$#columns bad field number $_\n";
-	}
-
-($aid,
-#$locality,
-$latitude,
-$longitude,
-$georef_source,
-$datum,
-$error_radius,
-$ER_units
-)=@columns;
+  $count++;
+  chomp;
+  @columns=split(/\t/,$_,100);
+  unless( $#columns==11-1){
+    print ERR "$#columns bad field number $_\n";
+  }
+  ($csid,
+   $aid,
+   #$locality,
+   $latitude,
+   $longitude,
+   $georef_source,
+   $datum,
+   $error_radius,
+   $ER_units,
+   $Georefer_name,
+   $Georef_date,
+   $Note
+  )=@columns;
 
 
 #####When not NULLing, all text fields must be enclosed by single quotes
 #####Numeric fields, including decimallatitude and decimallongitude, must have the quotes left off
 
-#####Hold lines removed from the print OUT
+##### Keep these line(s) for future use (removed from the print OUT):
 # fieldlocverbatim = '$locality',
 
-			print OUT <<EOP;
+  print OUT <<EOP;
 \\echo '$aid'
 update localitygroup
 set vlatitude = '$latitude', 
  vlongitude = '$longitude', 
  decimallatitude = $latitude,
  decimallongitude = $longitude, 
- localitysource =  '$georef_source',
  georefsource = '$georef_source', 
  geodeticdatum = '$datum', 
  coorduncertainty = '$error_radius', 
  coorduncertaintyunit = '$ER_units',
- georefremarks = NULL,
- georefencedby = NULL
+ georefremarks = '$Note',
+ georefencedby = '$Georefer_name'
 where id in
  (select lg.id FROM
   localitygroup lg, hierarchy h, collectionobjects_common cc
@@ -59,4 +73,4 @@ where id in
 
 EOP
 }
-print "Success. see georef2sql.out\n";
+print "Success. $count statements output\n";
